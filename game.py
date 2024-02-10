916cc1cfd2842bced50d86a0acb0815a144ae3e1
Kamran Azim <Kamazim121212@gmail.com>
2024-02-10 22:01:36 +0000

    Finished Version
    Reapted in while loop to keep game going until user losers, made it so correct guess is kept in to next round

--- game.py
+++ game.py
@@ -1,49 +1,113 @@
 from random import choice
 from game_stats import data
 from art import versus_art
+from os import system
 
 game_data = data
-score = 0 
+score = 0
 
-# Get two random people 
-a = choice(game_data)
-b = choice(game_data)
-while a == b:
+
+# Get two random people
+def get_celeb_data(): 
     a = choice(game_data)
     b = choice(game_data)
 
+    while a == b:
+        a = choice(game_data)
+        b = choice(game_data)
+
+    return a,b
+
+
 
 # Output Suitable versus message 
-print(f"Compare A: {a['name']} a {a['description']} from {a['country']}")
-print(versus_art)
-print(f"Against B: {b['name']} a {b['description']} from {b['country']}")
+def versus_message(a,b):
+    print(f"Compare A: {a['name']} a {a['description']} from {a['country']}")
+    print(versus_art)
+    print(f"Against B: {b['name']} a {b['description']} from {b['country']}")
+
+
+
+# Get User Input
+def user_input():
+    while True:
+        user_answer = input(f"Who has the most followers! (a or b): ").lower()
+
+        if user_answer in ["a", "b"]:
+            return user_answer
+        
+        else:
+            print("Please enter a valid input!")
+
 
 
 # Allow user to input either a or b regarding who has the higher follower count
-# If statement decide if user conitues depending if they were correct or not 
-# If wrong end game print who had higher
-# If correct increase score, continue with correct choice, remove wrong answer for 3 turns atleast 
+def game_logic(a,b):
 
-user_answer = input("Who has the most followers! (a or b): ").lower()
-answer = "a" if a["follower_count"] > b["follower_count"] else "b" if a["follower_count"] < b["follower_count"] else "Equal"
-ans_dict = {
-    "a": a["follower_count"],
-    "b": b["follower_count"] 
-}
+    global score
+    user_answer = user_input()
+    answer = "a" if a["follower_count"] > b["follower_count"] else "b" if a["follower_count"] < b["follower_count"] else "Equal"
 
-if answer == user_answer:
-    print(f"Correct! The answer was {answer} with {ans_dict[answer]},000,000")
-    score += 1
+    if answer == user_answer:
+        system("cls")
+        score += 1
+        print(f"Correct! Current Score: {score}")
 
-elif answer == "Equal":
-    print(f"Both celebrities have the same amount of followers! {answer} with {ans_dict[answer]},000,000")
-    score += 1 
+    else:
+        print(f"\nWrong! You Lose!")
+        print(f"Final Score: {score}")
+        return False
+    
+    return answer
 
-else:
-    print(f"Wrong! You Lose! The answer was {answer} with {ans_dict[answer]},000,000")
+
+
+# Get new pair of celebrities after correct guess
+def new_pair(pair,result):
+
+    ans_dict = {
+        "a": pair[0],
+        "b": pair[1] 
+    }
+
+    while True:
+
+        if result == "a":
+            a = ans_dict[result]
+            _,b = get_celeb_data() 
+
+            if a == b:
+                continue
+            else:
+                return a,b
+
+        else:
+            b = ans_dict[result]
+            a,_ = get_celeb_data() 
+
+            if a == b:
+                continue
+            else:
+                return a,b
 
 
 
 # Repeat Code until loser loses
+def main():
+    cont = True
+    celeb_pair = get_celeb_data()
 
+    while cont == True:
+        versus_message(*celeb_pair)
+        result = game_logic(*celeb_pair)
+        
+        if result == False:
+            print("Well Done!")
+            cont = False
+
+        else:
+            celeb_pair = new_pair(celeb_pair,result)
+
+
+main()
 
