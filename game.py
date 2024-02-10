5e76767648073ee2089fbd90e10c976e9f50c7f7
Kamran Azim <Kamazim121212@gmail.com>
2024-02-10 20:19:53 +0000

    Outputted Suitable Message

--- game.py
+++ game.py
@@ -1,20 +1,22 @@
 from random import choice
 from game_stats import data
+from art import versus_art
 game_data = data
 
-# To Do List
 
 # Get two random people 
-
 a = choice(game_data)
 b = choice(game_data)
 while a == b:
     a = choice(game_data)
     b = choice(game_data)
 
-print(f"{a=}\n{b=}")
 
 # Output Suitable versus message 
+print(f"Compare A: {a['name']} a {a['description']} from {a['country']}")
+print(versus_art)
+print(f"Against B: {b['name']} a {b['description']} from {a['country']}")
+
 # Allow user to input either a or b regarding who has the higher follower count
 # If statement decide if user conitues depending if they were correct or not 
 # If wrong end game print who had higher
