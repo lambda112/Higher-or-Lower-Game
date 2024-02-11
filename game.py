5c78202d2abf953815a96cbe1b4c1b1e9934de7e
Kamran Azim <Kamazim121212@gmail.com>
2024-02-11 01:58:05 +0000

    Improvements made based on feedback from reddit
    https://www.reddit.com/r/learnpython/comments/1ansdmm/how_can_i_improve_this_code_and_when_should_i_use/?sort=confidence

--- game.py
+++ game.py
@@ -1,113 +1,88 @@
-from random import choice
+import random
+import os
 from game_stats import data
 from art import versus_art
-from os import system
+
 
 game_data = data
-score = 0
-
-
-def get_celeb_data(): 
+def get_celeb_data(a = None): 
     """Return data on two random celebrities"""
-    a = choice(game_data)
-    b = choice(game_data)
+    if a:
+        b = random.choice(game_data)
+        while a and a == b:
+            b = random.choice(game_data)
 
-    while a == b:
-        a = choice(game_data)
-        b = choice(game_data)
+        return a,b
 
-    return a,b
-
-
+    return random.sample(game_data, k=2)
 
 
-def versus_message(a,b):
+
+def versus_message(celeb_a:list[dict], celeb_b:list[dict]):
     """Output Suitable versus message"""
-    print(f"Compare A: {a['name']} a {a['description']} from {a['country']}")
+    print(f"Compare A: {celeb_a['name']} a {celeb_a['description']} from {celeb_a['country']}")
     print(versus_art)
-    print(f"Against B: {b['name']} a {b['description']} from {b['country']}")
+    print(f"Against B: {celeb_b['name']} a {celeb_b['description']} from {celeb_b['country']}")
 
 
 
-
-def user_input():
+def user_input() -> str:
     """Get user input regarding thier answer a or b"""
     while True:
         user_answer = input(f"Who has the most followers! (a or b): ").lower()
-
         if user_answer in ["a", "b"]:
             return user_answer
-        
         else:
             print("Please enter a valid input!")
 
 
 
-def game_logic(a,b):
+def game_logic(a: list[dict], b:list[dict]) -> bool | str:
     "Compares user answer against actual answer and returns false if wrong or the answer if correct"
-    global score
     user_answer = user_input()
     answer = "a" if a["follower_count"] > b["follower_count"] else "b" if a["follower_count"] < b["follower_count"] else "Equal"
 
     if answer == user_answer:
-        system("cls")
-        score += 1
-        print(f"Correct! Current Score: {score}")
+        os.system("cls")
 
     else:
         print(f"\nWrong! You Lose!")
-        print(f"Final Score: {score}")
         return False
     
-    return answer
+    return answer, True
 
 
 
-
-def new_pair(pair,result):
+def new_pair(pair: list[dict],result:str) -> dict:
     "Takes the pair of celebrites and the correct answer to return a new pair including the correct result from the previous exchange"
     ans_dict = {
         "a": pair[0],
         "b": pair[1] 
     }
 
-    while True:
-
-        if result == "a":
-            a = ans_dict[result]
-            _,b = get_celeb_data() 
-
-            if a == b:
-                continue
-            else:
-                return a,b
-
-        else:
-            a = ans_dict[result]
-            _,b = get_celeb_data() 
-
-            if a == b:
-                continue
-            else:
-                return a,b
+    celeb_a, celeb_b = get_celeb_data(ans_dict[result])
+    return celeb_a, celeb_b
 
 
 
 def main():
     """Repeat Code until user loses"""
     cont = True
+    score = 0
     celeb_pair = get_celeb_data()
 
     while cont == True:
         versus_message(*celeb_pair)
         result = game_logic(*celeb_pair)
-        
+
         if result == False:
             print("Well Done!")
+            print(f"Final Score: {score}")
             cont = False
-
         else:
-            celeb_pair = new_pair(celeb_pair,result)
+            celeb_pair = new_pair(celeb_pair,result[0])
+            score += 1
+            print(f"Correct! Current Score: {score}")
 
 
 main()
